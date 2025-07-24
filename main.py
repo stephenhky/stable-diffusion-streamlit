c5243dabe15c1c5f663bbb6b1c65573625b6ac63
Kwan Yuet Stephen Ho <3810067+stephenhky@users.noreply.github.com>
2025-07-23 22:41:09 -0400

    updated images

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import streamlit as st
 
 from utils.sd import get_stable_diffusion_pipeline, generate_image
 
-base_model = st.text_input("Base model")
+base_model = st.radio(
+    "Base model",
+    ["runwayml/stable-diffusion-v1-5", "stabilityai/stable-diffusion-xl-base-1.0"]
+)
 lora_weights_path = st.text_input("LoRA weights path")
 vae_input_path = st.text_input("VAE path")
 cuda = st.checkbox("CUDA", False)
@@ -15,6 +18,9 @@ guidance_scale = st.number_input("Guidance scale", min_value=0.0, value=7.5)
 seed = st.number_input("Seed", min_value=-1, value=-1)
 
 nbimages = st.number_input("Number of images", min_value=1, value=1)
+
+positive_prompt = st.text_area("Positive prompt")
+negative_prompt = st.text_area("Negative prompt")
 
 if st.button("Generate image(s)!"):
     if seed == -1:
@@ -31,3 +37,16 @@ if st.button("Generate image(s)!"):
         cuda=cuda
     )
 
+    images = generate_image(
+        [positive_prompt]*nbimages,
+        [negative_prompt]*nbimages,
+        pipe,
+        height=height,
+        width=width,
+        steps=nbsteps,
+        guidance_scale=guidance_scale,
+        seed=seed
+    )
+
+    for image in images:
+        st.image(image)
